9914c6fd94fef583b44c330d20e0d413958d8689
Vamsikrishna Tamil selvan <krrishvamsi7@gmail.com>
2019-09-03 16:15:32 -0400

    password hashed and user model stored in database

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const UserSchema = mongoose.Schema({
-  user: {
+  name: {
     type: String,
     required: true
   },
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const { check, validationResult } = require("express-validator/check");
 const User = require("../models/User");
 
 //register a user
+
 router.post(
   "/",
   [
@@ -16,12 +18,35 @@ router.post(
       "please enter a password with atleast six characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send("passed");
+
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({ msg: "user already exists" });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      res.send("user saved");
+    } catch (err) {
+      console.log(err.message);
+      res.status(400).send("server error");
+    }
   }
 );
 
