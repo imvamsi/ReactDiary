5b4b9ad2809491016845300e26cb7c7d203cc7a0
Vamsikrishna Tamil selvan <krrishvamsi7@gmail.com>
2019-09-07 15:22:42 -0400

    jwt and middleware for user register added

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 const { check, validationResult } = require("express-validator/check");
 const User = require("../models/User");
 
@@ -42,7 +44,25 @@ router.post(
       user.password = await bcrypt.hash(password, salt);
 
       await user.save();
-      res.send("user saved");
+      //res.send("user saved");
+
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+
+      jwt.sign(
+        payload,
+        config.get("jwtSecret"),
+        {
+          expiresIn: 360000
+        },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.log(err.message);
       res.status(400).send("server error");
