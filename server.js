9598089a4d72b66cba3633700e69d0e5a9af8416
Vamsikrishna Tamil selvan <krrishvamsi7@gmail.com>
2019-09-15 00:08:26 -0400

    prepare to deploy

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
+const path = require("path");
 
-app.get("/", (req, res) => res.json({ msg: "welcome to the contact keeper" }));
+//app.get("/", (req, res) => res.json({ msg: "welcome to the contact keeper" }));
 
 //connect to the database
 connectDB();
@@ -16,6 +17,17 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/user"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+//serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
