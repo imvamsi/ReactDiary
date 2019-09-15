b813be105f020157f6ad10bc0dd8a60f7d9d2794
Vamsikrishna Tamil selvan <krrishvamsi7@gmail.com>
2019-09-15 01:07:34 -0400

    redeploy

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const connectDB = require("./config/db");
 const app = express();
 const path = require("path");
 
-//app.get("/", (req, res) => res.json({ msg: "welcome to the contact keeper" }));
-
 //connect to the database
 connectDB();
 
